679d82ce93f41685e37790017cce7bdd1b058d19
LakshyaSingh354 <lakshya.singh354@gmail.com>
2025-02-04 19:10:33 +0530

    add(Dockerfile): create Dockerfile for containerized application

--- src/app/config/auth.ts
+++ src/app/config/auth.ts
@@ -6,55 +6,55 @@ import { User } from "@/models/Users";
 import { User as NextAuthUser } from "next-auth";
 
 declare module "next-auth" {
-  interface CustomUser extends NextAuthUser {
-    id: string;
-  }
+	interface CustomUser extends NextAuthUser {
+		id: string;
+	}
 
-  interface Session {
-    user: CustomUser;
-  }
+	interface Session {
+		user: CustomUser;
+	}
 }
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-  pages: {
-    signOut: "/",
-  },
-  callbacks: {
-    async signIn({ user }) {
-      await connectDB();
-      const existingUser = await User.findOne({ email: user.email });
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+		}),
+	],
+	pages: {
+		signOut: "/",
+	},
+	callbacks: {
+		async signIn({ user }) {
+			await connectDB();
+			const existingUser = await User.findOne({ email: user.email });
 
-      if (!existingUser) {
-        await User.create({
-          name: user.name,
-          email: user.email,
-          avatar: user.image,
-        });
-      }
+			if (!existingUser) {
+				await User.create({
+					name: user.name,
+					email: user.email,
+					avatar: user.image,
+				});
+			}
 
-      return true;
-    },
-    async session({ session }) {
-      await connectDB();
-      const dbUser = await User.findOne({ email: session.user?.email });
+			return true;
+		},
+		async session({ session }) {
+			await connectDB();
+			const dbUser = await User.findOne({ email: session.user?.email });
 
-      if (dbUser) {
-        session.user = {
-          ...session.user,
-          id: dbUser._id.toString(),
-        };
-      }
+			if (dbUser) {
+				session.user = {
+					...session.user,
+					id: dbUser._id.toString(),
+				};
+			}
 
-      return session;
-    },
-    async redirect({ baseUrl }) {
-      return baseUrl + "/events";
-    },
-  },
+			return session;
+		},
+		async redirect({ baseUrl }) {
+			return baseUrl + "/home";
+		},
+	},
 };
