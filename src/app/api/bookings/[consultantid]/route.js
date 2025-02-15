8e986855e663aeaf6f1a83983b9cf36c61d1e293
LakshyaSingh354 <lakshya.singh354@gmail.com>
2025-02-15 22:02:51 +0530

    fix: update bookings API to use context for consultant ID retrieval

--- src/app/api/bookings/[consultantid]/route.js
+++ src/app/api/bookings/[consultantid]/route.js
@@ -4,18 +4,18 @@ import { Booking } from "@/models/Bookings"; // Import Booking model
 
 export async function GET(
 	req,
-	params 
+	context
 ) {
 	try {
 		await connectDB();
 
-		const { consultantid } = params;
-
+		const consultantid = context.params.consultantid;
 		// Fetch all bookings for the given event ID
+		console.log(consultantid)
 		const bookings = await Booking.find({ consultant: consultantid }).select(
 			"startTime endTime"
 		);
-
+		console.log(bookings);
 		return NextResponse.json(bookings, { status: 200 });
 	} catch (error) {
 		console.error("Error fetching bookings:", error);
